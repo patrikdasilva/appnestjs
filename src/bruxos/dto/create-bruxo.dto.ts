734ddc022fc698e5e38004fbc187e95aba4e78b1
patrikschwindt <pschwindt@xcapitalbank.com.br>
2023-10-05 23:24:33 -0300

    aplicando class validator

--- src/bruxos/dto/create-bruxo.dto.ts
+++ src/bruxos/dto/create-bruxo.dto.ts
@@ -1,5 +1,11 @@
+import { IsNotEmpty } from "class-validator";
+
 export class CreateBruxoDto {
+    @IsNotEmpty({
+        message: "Propriedade nome é obrigatório"
+    })
     name: string;
+
     house: string;
     patrono: string;
 }
--- src/bruxos/dto/update-bruxo.dto.ts
+++ src/bruxos/dto/update-bruxo.dto.ts
@@ -1,8 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBruxoDto } from './create-bruxo.dto';
+import { IsNotEmpty } from 'class-validator';
 
 export class UpdateBruxoDto extends PartialType(CreateBruxoDto) {
+
+    @IsNotEmpty()
     name: string;
+    
     house: string;
     patrono: string;
 }
